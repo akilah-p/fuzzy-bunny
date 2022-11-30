d7278161e9d9970d386a2a2f951a5f3999c1a801
Akilah Powell <atpjun@gmail.com>
2022-11-30 15:51:24 -0400

    created the event form event listener for family and create bunny in create.js

--- create/create.js
+++ create/create.js
@@ -5,15 +5,22 @@ const logoutButton = document.getElementById('logout');
 
 form.addEventListener('submit', async (e) => {
     // prevent default
-
+    e.preventDefault();
     // get the name and family id from the form
+    const formData = new FormData(form);
+    const familyId = formData.get('family-id');
+    const name = formData.get('bunny-name');
 
     // use createBunny to create a bunny with this name and family id
+    await createBunny({
+        name: name,
+        family_id: familyId
+    });
 
     form.reset();
 });
 
-window.addEventListener('load', async () => {
+window.addEventListener('load', async () =>
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
     // go get the families from supabase
